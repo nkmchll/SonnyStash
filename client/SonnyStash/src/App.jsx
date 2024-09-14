a41a9349336099a39a3e2ce5f442c8da2b36dc6e
Nika Calapini <nmc2003@nmsu.edu>
2024-09-14 04:00:52 -0600

    set up main app structure with routing and context provider

---
+++ client/SonnyStash/src/App.jsx
@@ -0,0 +1,31 @@
+import './App.css'
+import {Routes, Route} from 'react-router-dom'
+import Navbar from '../src/components/navbar'
+import Home from './pages/home' 
+import Register from './pages/register'
+import Login from './pages/login'
+import Dashboard from './pages/dashboard'
+import axios from 'axios'
+import {Toaster} from 'react-hot-toast'
+import { UserContextProvider } from '../context/userContext'
+
+
+axios.defaults.baseURL = 'http://localhost:8000';
+axios.defaults.withCredentials = true;
+
+function App() {
+  return (
+    <UserContextProvider>
+      <Navbar></Navbar>
+      <Toaster position='bottom-right' toastOptions = {{duration: 2000}}></Toaster>
+      <Routes>
+        <Route path = '/' element={<Home/>}/>
+        <Route path = '/register' element={<Register></Register>}/>
+        <Route path = '/login' element={<Login></Login>}/>
+        <Route path = '/dashboard' element={<Dashboard/>}/>
+      </Routes>
+    </UserContextProvider>
+  )
+}
+
+export default App

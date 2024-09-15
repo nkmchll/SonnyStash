256db5f2f9dd9e581d60193960a1e8c6bf275fab
Nika Calapini <nmc2003@nmsu.edu>
2024-09-14 20:21:50 -0600

    integrate themeprovider and usercontext with routing and axios setup

--- client/SonnyStash/src/App.jsx
+++ client/SonnyStash/src/App.jsx
@@ -1,4 +1,4 @@
-import './App.css'
+import './index.css'
 import {Routes, Route} from 'react-router-dom'
 import Navbar from '../src/components/navbar'
 import Home from './pages/home' 
@@ -8,23 +8,30 @@ import Dashboard from './pages/dashboard'
 import axios from 'axios'
 import {Toaster} from 'react-hot-toast'
 import { UserContextProvider } from '../context/userContext'
+import { themeSettings } from './theme'
+import { ThemeProvider, createTheme } from '@mui/material/styles'
 
 
 axios.defaults.baseURL = 'http://localhost:8000';
 axios.defaults.withCredentials = true;
 
+const theme = createTheme(themeSettings());
+
 function App() {
   return (
-    <UserContextProvider>
-      <Navbar></Navbar>
-      <Toaster position='bottom-right' toastOptions = {{duration: 2000}}></Toaster>
-      <Routes>
-        <Route path = '/' element={<Home/>}/>
-        <Route path = '/register' element={<Register></Register>}/>
-        <Route path = '/login' element={<Login></Login>}/>
-        <Route path = '/dashboard' element={<Dashboard/>}/>
-      </Routes>
-    </UserContextProvider>
+    <ThemeProvider theme={theme}>
+      <UserContextProvider>
+        <Navbar></Navbar>
+        <Toaster position='bottom-right' toastOptions = {{duration: 2000}}></Toaster>
+        <Routes>
+          <Route path = '/' element={<Home/>}/>
+          <Route path = '/register' element={<Register></Register>}/>
+          <Route path = '/login' element={<Login></Login>}/>
+          <Route path = '/dashboard' element={<Dashboard/>}/>
+        </Routes>
+      </UserContextProvider>
+    </ThemeProvider>
+    
   )
 }
 

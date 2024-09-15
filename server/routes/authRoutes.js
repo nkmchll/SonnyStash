6f67192bb9bcf8fee4b5212a905857fcb41d010b
Nika Calapini <nmc2003@nmsu.edu>
2024-09-15 01:24:08 -0600

    add wishlist routes post and get

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const cors = require('cors');
-const {test, registerUser, loginUser, getProfile} = require('../controllers/authController')
+const {test, registerUser, loginUser, getProfile, addToWishList, getWishlist} = require('../controllers/authController')
 const {logoutUser} = require('../controllers/authController')
 
 // middleware
@@ -12,11 +12,18 @@ router.use(
     })
 )
 
+// test route
 router.get('/', test);
+
+// user routes
 router.post('/register', registerUser);
-router.post('/login', loginUser)
-router.get('/profile', getProfile)
-router.post('/logout', logoutUser)
+router.post('/login', loginUser);
+router.get('/profile', getProfile);
+router.post('/logout', logoutUser);
+
+// wishlist routes
+router.post('/wishlist', addToWishList);
+router.get('/wishlist/:userId', getWishlist);
 
 module.exports = router
 

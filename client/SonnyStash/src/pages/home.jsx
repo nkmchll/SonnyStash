5c2ee7504cd18765149562fbf365eb082f8b5266
Nika Calapini <nmc2003@nmsu.edu>
2024-09-14 20:23:08 -0600

    added styled components with navigation buttons to home page

--- client/SonnyStash/src/pages/home.jsx
+++ client/SonnyStash/src/pages/home.jsx
@@ -1,9 +1,50 @@
-import React from 'react'
+import React from 'react';
+import { Box, Typography, useTheme, Button } from '@mui/material';
+import { useNavigate } from 'react-router-dom';
 
-export default function Home() {
+
+const Home = () => {
+  const theme = useTheme();
+  const navigate = useNavigate();
+
   return (
-    <div>
-      Home
-    </div>
-  )
-}
+    <Box
+      width="100%"
+      minHeight="100vh"
+      sx={{ backgroundColor: theme.palette.main.pink }} 
+      display="flex"
+      justifyContent="center"
+      alignItems="center"
+      flexDirection="column"
+    >
+      <Typography variant="h2" sx={{ fontWeight: 900 }} color={theme.palette.main.beige}>
+        WELCOME TO
+      </Typography>
+      
+      <Typography variant="h1" color={theme.palette.main.beige} sx={{ WebkitTextStroke: "2.5px black" }} padding="20px">
+        SonnyStash
+      </Typography>
+      
+      <Typography variant="h3" color={theme.palette.main.beige} sx={{ fontWeight: 700 }}>
+        YOUR ULTIMATE DESTINATION FOR <span style={{ color: '#000' }}>SONNY ANGEL</span> COLLECTIONS!
+      </Typography>
+
+      <Box display="flex" flexDirection="column" padding="40px">
+        <Button
+          sx={{ backgroundColor: theme.palette.main.black, color: theme.palette.main.beige, mb: 2, borderRadius: '50px', width: '100px' }}
+          variant="contained" onClick={() => navigate('/login')}
+        >
+          LOGIN
+        </Button>
+        <Button
+          sx={{ backgroundColor: theme.palette.main.black, color: theme.palette.main.beige, mb: 2, borderRadius: '50px', width: '100px' }}
+          variant="contained"  onClick={() => navigate('/register')}
+        >
+          SIGNUP
+        </Button>
+      </Box>
+    </Box>
+  );
+};
+
+export default Home;
